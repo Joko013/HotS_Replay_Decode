715a7075243857b5dec2a7c83d4b730a964fb031
Joko013 <30841710+Joko013@users.noreply.github.com>
2018-02-21 18:50:28 +0100

    Export/import udate

--- Replay.py
+++ Replay.py
@@ -7,9 +7,56 @@ Created on Thu Dec 07 17:32:31 2017
 from mpyq import MPQArchive
 from heroprotocol27 import protocol46889 as protocol
 from importlib import import_module
+import os
+import csv
+#import mpyq module, replay file, decode build number from protocol
+import mpyq
+import pandas as pd
 
 
-class Replay(object):
+class Replays_list(object):
+    
+    def __init__(self):
+        self._all_stats = []
+        self._replays_path = os.path.join(os.getcwd(),'Replays')
+        self._replays = []
+        for (dirpath, dirnames, filenames) in os.walk(self._replays_path):
+            self._replays.extend(filenames)
+            break
+            
+    def replay_stats(self):
+        
+        for replay in self._replays:
+            filename = os.path.join(self._replays_path, replay)
+            to_decode = Replay_file(filename)
+            #all_stats.append({'name': replay, 'gameloop': 0 })
+            self._all_stats.append(to_decode.decode_replay())
+        #return self._all_stats
+    
+    def export_stats_to_xlsx(self):
+        self.replay_stats()
+        
+        self._df = pd.DataFrame(columns = ['gameloop', 'name'])
+        for item in self._all_stats:
+            app = pd.DataFrame(item, columns = ['gameloop', 'name'])
+            self._df = self._df.append(app)
+
+        writer = pd.ExcelWriter('death_stats.xlsx')
+        self._df.to_excel(writer,'Sheet1')
+        writer.save()
+        return self._df
+    
+    @property
+    def stats(self):
+        if self._all_stats == []:
+            self.replay_stats()
+        else:
+            pass
+        return self._all_stats
+            
+   
+
+class Replay_file(object):
     
     def __init__(self, filename):
         self.build = None
